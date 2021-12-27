e8888bea0d155056cdaaa79ce176e04205375de4
sexydan322 <robert.third007@gmail.com>
2021-12-27 18:37:28 +0200

    new

--- homework_11_1.py
+++ homework_11_1.py
@@ -1,25 +1,24 @@
-class Counter:
+class counter:
 
     def __init__(self, minimal: int, maximal: int):
         self.minimal = minimal
         self.maximal = maximal
-        self.current = minimal
+        self.actual = minimal
 
-    def increment(self):
-        self.current += 1
-        if self.current > self.maximal:
-            self.current = self.minimal
+    def upp(self):
+        self.actual += 1
+        if self.actual > self.maximal:
+            self.actual = self.minimal
 
     def __repr__(self):
-        return f'Counter(minimal={self.minimal}, maximal={self.maximal}), current position={self.current}'
+        return f'counter(minimal={self.minimal}, maximal={self.maximal}), actual position={self.actual}'
 
 
 if __name__ == '__main__':
-    counter = Counter(0, 10)
-    print(counter)
+    counter1 = counter(0, 10)
+    print(counter1)
     for _ in range(10):
-        counter.increment()
-        print(counter.current)
-    print('Check resetting')
-    counter.increment()
-    print(counter.current)
+        counter1.upp()
+        print(counter1.actual)
+    counter1.upp()
+    print(counter1.actual)
